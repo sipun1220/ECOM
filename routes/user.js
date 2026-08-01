03423e96d83a944f4ef146dbbd866fb875cd96e0
sipun das <dsipun8484@gmail.com>
2026-08-01 21:18:06 +0530

    heio

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
+const User = require('../models/user.js');
 const wrapAsync = require('../utils/wrapAsync.js');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
